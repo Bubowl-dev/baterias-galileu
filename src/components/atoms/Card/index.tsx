c32331393f3f054fbe29021b14acd099304d4db0
Gustavo pontes <gustavo.4dev@gmail.com>
2024-12-04 15:02:37 -0300

    fix: update Card component link structure for services

--- src/components/atoms/Card/index.tsx
+++ src/components/atoms/Card/index.tsx
@@ -5,7 +5,7 @@ import CardProps from './props';
 const Card: FC<CardProps> = ({ title, text, link, img }) => {
   return (
     <S.Card>
-      <S.CardLink href={link} title={text}>
+      <S.CardLink href={`/servicos/${link}`} title={text}>
         <S.Img src={img.includes('/posts') ? img : `/posts/${img}`} alt={title} width={335} height={246} />
         <S.Title>{title}</S.Title>
         <S.Text>{text}</S.Text>
